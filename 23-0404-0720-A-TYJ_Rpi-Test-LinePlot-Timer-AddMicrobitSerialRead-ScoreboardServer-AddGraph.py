8914cd290bdd1df6adf294e6c8e287bda090caef
eande-jasonc1025 <eande.jasonc@gmail.com>
2023-04-30 05:50:57 -0700

    23-0430-0600 TYJ finally works 'arrayList of Dictionary Pairs'  : )+

--- 23-0404-0720-A-TYJ_Rpi-Test-LinePlot-Timer-AddMicrobitSerialRead-ScoreboardServer-AddGraph.py
+++ 23-0404-0720-A-TYJ_Rpi-Test-LinePlot-Timer-AddMicrobitSerialRead-ScoreboardServer-AddGraph.py
@@ -8,6 +8,8 @@ import time
 import serial
 from datetime import datetime
 
+import random
+
 ser = serial.Serial(
         ##jwc o port='/dev/ttyACM0',
         ##jwc o port='COM3',
@@ -31,6 +33,9 @@ y2Value = [0,0,0]
 
 ###jwc n scoreboard_BotsAll_StringFull_ArrayList_2D =[{'#':0,'l':1,'m':2},{'#':10,'l':11,'m':12},{'#':20,'l':21,'m':22}]
 
+###jwc n scoreboard_BotsAll_StringFull_ArrayList_2D =[{'#':0,'l':1,'m':2},{'#':10,'l':11,'m':12},{'#':20,'l':21,'m':22}]
+
+
 _debug_Show_Priority_Hi_Bool = True
 
 ###jwc o line_plot = ui.line_plot(n=2, limit=20, figsize=(3, 2), update_every=5) \
@@ -43,46 +48,47 @@ line_plot = ui.line_plot(n=2, limit=40, figsize=(6, 4), update_every=1) \
 
 def update_line_plot() -> None:
 
-    now,x,y1,y2 = 0, 0, 0, 0
+    now,network_DataMessage_Rcvd_Bytes,y1,y2 = 0, 0, 0, 0
 
-    x=ser.readline()
-    if x:
+    network_DataMessage_Rcvd_Bytes = ser.readline()
+    if network_DataMessage_Rcvd_Bytes:
         dt = datetime.now()
         datestamp = str(dt)[:16]
-        ###jwc o temp, light = x.decode().split(':')
-        ###jwc 23-0310-1120 y id, te, li, co = x.decode().split(',')
-        ###jwc n id, te, li, co, m1, m2, m3, m4 = x.decode().split('|')
+        ###jwc o temp, light = network_DataMessage_Rcvd_Bytes.decode().split(':')
+        ###jwc 23-0310-1120 y id, te, li, co = network_DataMessage_Rcvd_Bytes.decode().split(',')
+        ###jwc n id, te, li, co, m1, m2, m3, m4 = network_DataMessage_Rcvd_Bytes.decode().split('|')
     
         ###jwc o newData = [datestamp,temp,light]
         ###jwc 23-0310-1120 y newData = [datestamp, id, te, li, co]
         ###jwc n newData = [datestamp, id, te, li, co, m1, m2, m3, m4]
-        newData = [datestamp, x]
+        newData = [datestamp, network_DataMessage_Rcvd_Bytes]
     
         print(newData)
 
-        ###jwc o network_DataNew_Message_Str = x
-        network_DataNew_Message_Str = str(x)
+        ###jwc o network_DataMessage_Rcvd_Str = network_DataMessage_Rcvd_Bytes
+        network_DataMessage_Rcvd_Str = str(network_DataMessage_Rcvd_Bytes)
         ##jwc workaround for '\r\n' tail
-        network_DataNew_Message_Str = network_DataNew_Message_Str[0:len(network_DataNew_Message_Str)-10]
+        network_DataMessage_Rcvd_Str = network_DataMessage_Rcvd_Str[0:len(network_DataMessage_Rcvd_Str)-10]
 
         if _debug_Show_Priority_Hi_Bool:
             ###jwc o print replaced by 'print'
             print("* A: Raw String: ")
-            print("> " + str(network_DataNew_Message_Str))
+            print("> " + str(network_DataMessage_Rcvd_Str))
         if True:
             scoreboard_DataNumNew_ArrayList = []
-            ###jwc y scoreboard_DataStrNew_ArrayList = network_DataNew_Message_Str.split("|")
-            scoreboard_DataStrNew_ArrayList = network_DataNew_Message_Str.split(",")
+            ###jwc y scoreboard_DataStrNew_ArrayList = network_DataMessage_Rcvd_Str.split("|")
+            scoreboard_DataStrNew_ArrayList = network_DataMessage_Rcvd_Str.split(",")
 
-            for value in scoreboard_DataStrNew_ArrayList:
-                ###jwc o value22 = value.substr(value.index_of(":") + 1, len(value))
-                ###jwc o value22 = value[value.index_of(":") + 1:len(value)]
-                value22 = value[value.index(":") + 1:len(value)]
-                scoreboard_DataNumNew_ArrayList.append(int(value22))
+            for key_Value_Pair in scoreboard_DataStrNew_ArrayList:
+                ###jwc o key_Value_Pair__Value = key_Value_Pair.substr(key_Value_Pair.index_of(":") + 1, len(key_Value_Pair))
+                ###jwc o key_Value_Pair__Value = key_Value_Pair[key_Value_Pair.index_of(":") + 1:len(key_Value_Pair)]
+                ## * Skip past "b'" prefix
+                key_Value_Pair__Key = key_Value_Pair[2 : key_Value_Pair.index(":")-1]
+                key_Value_Pair__Value = key_Value_Pair[key_Value_Pair.index(":") + 1:len(key_Value_Pair)]
+                scoreboard_DataNumNew_ArrayList.append(int(key_Value_Pair__Value))
                 if _debug_Show_Priority_Hi_Bool:
-                    print("* B: Parsed Key:Value:")
-                    print(value)
-                    print(value22)
+                    print("* B: Parsed Key:key_Value_Pair:")
+                    print(key_Value_Pair, key_Value_Pair__Key, key_Value_Pair__Value)
                     print("" + str((scoreboard_DataNumNew_ArrayList[len(scoreboard_DataNumNew_ArrayList) - 1])))
                     print(scoreboard_DataNumNew_ArrayList)
 
@@ -144,14 +150,14 @@ def update_line_plot() -> None:
 
 
                     now = datetime.now()
-                    x = now.timestamp()
-                    ###jwc o y1 = np.sin(x)
-                    ###jwc o y2 = np.cos(x)
+                    network_DataMessage_Rcvd_Bytes = now.timestamp()
+                    ###jwc o y1 = np.sin(network_DataMessage_Rcvd_Bytes)
+                    ###jwc o y2 = np.cos(network_DataMessage_Rcvd_Bytes)
                     
                     ###jwc y print("*** ***" + str(y1) +" "+ str(y2))
                     ###jwc y line_plot.push([now], [[y1], [y2]])  
                     
-                ###jwc n scoreboard_BotsAll_StringFull_ArrayList_2D[index2] = x            
+                ###jwc n scoreboard_BotsAll_StringFull_ArrayList_2D[index2] = network_DataMessage_Rcvd_Bytes            
 
                 index2 += 1
         if not (scoreboard_Bot_Found_Bool):
@@ -161,17 +167,17 @@ def update_line_plot() -> None:
         
 
         ###jwc n prints twice: 2nd 0: now = datetime.now()
-        ###jwc n prints twice: 2nd 0: x = now.timestamp()
-        ###jwc n prints twice: 2nd 0: ###jwc o y1 = np.sin(x)
-        ###jwc n prints twice: 2nd 0: ###jwc o y2 = np.cos(x)
+        ###jwc n prints twice: 2nd 0: network_DataMessage_Rcvd_Bytes = now.timestamp()
+        ###jwc n prints twice: 2nd 0: ###jwc o y1 = np.sin(network_DataMessage_Rcvd_Bytes)
+        ###jwc n prints twice: 2nd 0: ###jwc o y2 = np.cos(network_DataMessage_Rcvd_Bytes)
         ###jwc n prints twice: 2nd 0: 
         ###jwc n prints twice: 2nd 0: print("*** ***" + str(y1) +" "+ str(y2))
         ###jwc n prints twice: 2nd 0: line_plot.push([now], [[y1], [y2]])
 
     ###jwc 23-0423-2050 ? w = datetime.now()
     ###jwc 23-0423-2050 ? = now.timestamp()
-    ###jwc 23-0423-2050 ? #jwc o y1 = np.sin(x)
-    ###jwc 23-0423-2050 ? #jwc o y2 = np.cos(x)
+    ###jwc 23-0423-2050 ? #jwc o y1 = np.sin(network_DataMessage_Rcvd_Bytes)
+    ###jwc 23-0423-2050 ? #jwc o y2 = np.cos(network_DataMessage_Rcvd_Bytes)
 
     ###jwc 23-0423-2050 ? int("*** ***" + str(y1Value[1]) +" "+ str(y1Value[2]))
     ###jwc 23-0423-2050 ? ne_plot.push([now], [[y1Value[1]], [y1Value[2]]])  
@@ -204,40 +210,97 @@ line_updates_02 = ui.timer(0.1, update_line_plot_02, active=True)
 ##jwc n )
 
 ##jwc n ##jwc READ ONCE AND PRINTED ONCE THEN SAME 'NO PERMISSION FAIL'
-##jwc n x=ser.readline()
-##jwc n if x:
+##jwc n network_DataMessage_Rcvd_Bytes=ser.readline()
+##jwc n if network_DataMessage_Rcvd_Bytes:
 ##jwc n     dt = datetime.now()
 ##jwc n     datestamp = str(dt)[:16]
-##jwc n     ###jwc o temp, light = x.decode().split(':')
-##jwc n     ###jwc 23-0310-1120 y id, te, li, co = x.decode().split(',')
-##jwc n     ###jwc n id, te, li, co, m1, m2, m3, m4 = x.decode().split('|')
+##jwc n     ###jwc o temp, light = network_DataMessage_Rcvd_Bytes.decode().split(':')
+##jwc n     ###jwc 23-0310-1120 y id, te, li, co = network_DataMessage_Rcvd_Bytes.decode().split(',')
+##jwc n     ###jwc n id, te, li, co, m1, m2, m3, m4 = network_DataMessage_Rcvd_Bytes.decode().split('|')
 ##jwc n 
 ##jwc n     ###jwc o newData = [datestamp,temp,light]
 ##jwc n     ###jwc 23-0310-1120 y newData = [datestamp, id, te, li, co]
 ##jwc n     ###jwc n newData = [datestamp, id, te, li, co, m1, m2, m3, m4]
-##jwc n     newData = [datestamp, x]
+##jwc n     newData = [datestamp, network_DataMessage_Rcvd_Bytes]
 ##jwc n     print(newData)
 ##jwc n 
 
 
+###jwc o grid = ui.aggrid({
+###jwc o     'columnDefs': [
+###jwc o         {'headerName': 'Name', 'field': 'name'},
+###jwc o         {'headerName': 'Age', 'field': 'age'},
+###jwc o         {'headerName': 'Weight', 'field': 'weight'},
+###jwc o     ],
+###jwc o     ###jwc n 'rowData': [
+###jwc o     ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[0],
+###jwc o     ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[1],
+###jwc o     ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[2],
+###jwc o     ###jwc n ],
+###jwc o     'rowData': [
+###jwc o         {'name': 'Alice', 'age': 10, 'weight':100},
+###jwc o         {'name': 'Bob', 'age': 20, 'weight':200},
+###jwc o         {'name': 'Carol', 'age': 30, 'weight':300},
+###jwc o     ],
+###jwc o     'rowSelection': 'multiple',
+###jwc o }).classes('max-h-40')
+
+dictionary_Scoreboard_BotsAll_Ids = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+
+###jwc n dictionary_Scoreboard_BotsAll_Value_Default = {'botid':'', 'light_lastdelta':'', 'light_total':'', 'magnet_lastdelta':'', 'magnet_total':''},
+dictionary_Scoreboard_BotsAll_Value_Default = {'botid':0, 'light_lastdelta':0, 'light_total':0, 'magnet_lastdelta':0, 'magnet_total':0},
+
+###jwc n    'rowData' : dict.fromkeys( dictionary_Scoreboard_BotsAll_Ids, dictionary_Scoreboard_BotsAll_Value_Default),
+
+###jwc n rowData' :[
+###jwc n         {1:dictionary_Scoreboard_BotsAll_Value_Default},
+###jwc n         {2:dictionary_Scoreboard_BotsAll_Value_Default},
+###jwc n         {3:dictionary_Scoreboard_BotsAll_Value_Default},
+###jwc n     ],
+
+    ###jwc y 'rowData' :[
+    ###jwc y     {'row#':1, 'botid':11, 'light_lastdelta':12, 'light_total':13, 'magnet_lastdelta':14, 'magnet_total':15},
+    ###jwc y     {'row#':2, 'botid':21, 'light_lastdelta':22, 'light_total':23, 'magnet_lastdelta':24, 'magnet_total':25},
+    ###jwc y     {'row#':3, 'botid':31, 'light_lastdelta':32, 'light_total':33, 'magnet_lastdelta':34, 'magnet_total':35},    
+    ###jwc y ],
+
+###jwc n rowData_Details =
+###jwc n     {
+###jwc n         '1': {'botid':11, 'light_lastdelta':12, 'light_total':13, 'magnet_lastdelta':14, 'magnet_total':15},
+###jwc n         '2': {'botid':21, 'light_lastdelta':22, 'light_total':23, 'magnet_lastdelta':24, 'magnet_total':25},
+###jwc n         '3': {'botid':31, 'light_lastdelta':32, 'light_total':33, 'magnet_lastdelta':34, 'magnet_total':35},
+###jwc n     }
+
+###jwc n     'rowData' : rowData_Details,
+###jwc n rowData_Details = {
+###jwc n     {'row#':1, 'botid':11, 'light_lastdelta':12, 'light_total':13, 'magnet_lastdelta':14, 'magnet_total':15},
+###jwc n     {'row#':2, 'botid':21, 'light_lastdelta':22, 'light_total':23, 'magnet_lastdelta':24, 'magnet_total':25},
+###jwc n     {'row#':3, 'botid':31, 'light_lastdelta':32, 'light_total':33, 'magnet_lastdelta':34, 'magnet_total':35},    
+###jwc n }
+
 grid = ui.aggrid({
     'columnDefs': [
-        {'headerName': 'Name', 'field': 'name'},
-        {'headerName': 'Age', 'field': 'age'},
-        {'headerName': 'Weight', 'field': 'weight'},
+        {'headerName': 'Row#', 'field': 'row#'},
+        {'headerName': 'BotId', 'field': 'botid'},
+        {'headerName': 'Light_LastDelta', 'field': 'light_lastdelta'},
+        {'headerName': 'Light_Total', 'field': 'light_total'},
+        {'headerName': 'Magnet_LastDelta', 'field': 'magnet_lastdelta'},
+        {'headerName': 'Magnet_Total', 'field': 'magnet_total'},
     ],
-    ###jwc n 'rowData': [
-    ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[0],
-    ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[1],
-    ###jwc n     scoreboard_BotsAll_StringFull_ArrayList_2D[2],
-    ###jwc n ],
-    'rowData': [
-        {'name': 'Alice', 'age': 10, 'weight':100},
-        {'name': 'Bob', 'age': 20, 'weight':200},
-        {'name': 'Carol', 'age': 30, 'weight':300},
+    'rowData' :[
+        {'row#':1, 'botid':11, 'light_lastdelta':12, 'light_total':13, 'magnet_lastdelta':14, 'magnet_total':15},
+        {'row#':2, 'botid':21, 'light_lastdelta':22, 'light_total':23, 'magnet_lastdelta':24, 'magnet_total':25},
+        {'row#':3, 'botid':31, 'light_lastdelta':32, 'light_total':33, 'magnet_lastdelta':34, 'magnet_total':35},    
     ],
     'rowSelection': 'multiple',
 }).classes('max-h-40')
+
+
+    ###jwc y grid.options['rowData'][1]['age'] += 1
+    ###jwc y grid.options['rowData'][2]['weight'] += 2
+
+tenp1 = 5
+temp2 = 10
 
 def updateGrid():
     ###jwc n grid.options[
@@ -247,9 +310,10 @@ def updateGrid():
     ###jwc n     {'name': 'Carol', 'age': 52},
     ###jwc n]] 
     ###jwc y grid.options['rowData'][0]['age'] += 1
-    grid.options['rowData'][1]['age'] += 1
+    grid.options['rowData'][1]['light_lastdelta'] += tenp1
     ###jwc n grid.options['rowData']['Carol']['age'] = random(9)
-    grid.options['rowData'][2]['weight'] += 2
+    grid.options['rowData'][2]['light_total'] += temp2
+    grid.options['rowData'][0]['magnet_lastdelta'] = random.randint(1,100)
 
     grid.update()
 
